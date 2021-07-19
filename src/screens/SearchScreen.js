76cf0c0b5e4241d40f20367186b5d1557c609e93
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-19 15:25:38 +0530

    Added search and API integration

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchBar from "../components/SearchBar";
-
+import yelp from "../api/yelp";
 const SearchScreen = () => {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
 
+  const makeaSearch = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term: search,
+        location: "dallas",
+      },
+    });
+    console.log(response.data.businesses);
+    setResults(response.data.businesses);
+  };
   return (
     <View>
       <SearchBar
         term={search}
         onTermChange={(newTerm) => setSearch(newTerm)}
-        onTermSubmit={() => console.log("Term submitted")}
+        onTermSubmit={makeaSearch}
       />
       <Text>{search}</Text>
+      <Text>You have around {results.length} restaurants for your search!</Text>
     </View>
   );
 };
