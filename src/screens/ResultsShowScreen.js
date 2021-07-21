c4ddb2fed01bbb2adb49bd58e5471a5f262e169f
Adithya Kamath <adithyakamath96@gmail.com>
2021-07-21 22:22:46 +0530

    Bug of images not being rendered. need to check

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
@@ -9,19 +9,39 @@ const ResultsShowScreen = ({ navigation }) => {
   const getResult = async (id) => {
     const response = await yelp.get(`/${id}`);
     setResult(response.data);
-    
   };
   useEffect(() => {
     getResult(id);
-    console.log(result);
   }, []);
+
+  if (!result) {
+    return null;
+  }
   return (
     <View>
       <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          <Image
+            style={styles.image}
+            source={{
+              uri: item,
+            }}
+          />;
+        }}
+      />
     </View>
   );
 };
 
 export default ResultsShowScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 300,
+    height: 400,
+    borderRadius: 5
+  },
+});
